0525a4ee2e42592f6f7ac4adb9370076e1b57622
corey <corey2.smith@live.uw.ac.uk>
2021-08-09 00:06:00 +0100

    Music and Rtpc Music Script
    need to get in scene control

---
+++ Assets/Scripts/AudioScripts/MusicIntensity.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using System;
+
+public class MusicIntensity : MonoBehaviour
+{
+    StudioEventEmitter MusicTense;
+
+
+
+    // Start is called before the first frame update
+        StudioEventEmitter MusicEmitter;
+   
+        [Range(0, 100)] [SerializeField] int calm = 100;
+        [Range(0, 50)] [SerializeField] int fade = 50;
+        [Range(0, 100)] [SerializeField] int gondola_idle = 100;
+
+        int prevcalm = 100;
+        int prevfade = 50;
+        int prev = 100;
+
+
+    private void Start()
+    {
+        MusicEmitter = GetComponent<StudioEventEmitter>();
+
+        MusicEmitter.SetParameter("calm", (float)calm);
+        MusicEmitter.SetParameter("fade", (float)fade);
+        MusicEmitter.SetParameter("gondola_idle", (float)gondola_idle);
+
+    }
+
+
+        private void Update()
+        {
+            if (calm != prevcalm)
+            {
+                MusicEmitter.SetParameter("calm", (float)calm);
+                prevcalm = calm;
+            }
+
+            if (fade != prevfade)
+            {
+                MusicEmitter.SetParameter("fade", (float)fade);
+                fade = prevfade;
+            }
+
+            if (gondola_idle != calm)
+            {
+                MusicEmitter.SetParameter("fade", (float)fade);
+                prevfade = fade;
+            }
+        }
+    }
+

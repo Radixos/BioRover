e7f1b171e1884de6888269050e52628ac5b67316
corey <corey2.smith@live.uw.ac.uk>
2021-08-17 14:37:21 +0100

    Last Audio Bits

---
+++ Assets/Scripts/BubblesJump.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+using System.Diagnostics;
+
+
+public class BubblesJump : MonoBehaviour
+{
+    FMOD.Studio.EventInstance Jumpsound;
+    [EventRef]
+    public string eventName;
+
+    private void Start()
+    {
+        Jumpsound = RuntimeManager.CreateInstance(eventName);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jumpsound.start();
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jumpsound.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            Jumpsound.release();
+        }
+
+    }
+
+
+}
